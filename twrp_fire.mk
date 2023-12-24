27760b3403941d1f1e6f6d1e5086575e4813dfcc
YudhoPatrianto <kydh01123@gmail.com>
2023-12-24 21:00:42 +0700

    bringup

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from fire device
 $(call inherit-product, device/xiaomi/fire/device.mk)
 
 PRODUCT_DEVICE := fire
-PRODUCT_NAME := omni_fire
+PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := fire
 PRODUCT_MANUFACTURER := xiaomi
