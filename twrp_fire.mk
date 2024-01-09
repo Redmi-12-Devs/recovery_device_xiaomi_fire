3ee2fee9247dd7176f052acefd12bafc9c1940ae
YudhoPatrianto <kydh01123@gmail.com>
2024-01-10 01:03:49 +0700

    fire: remodified
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -7,7 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -20,10 +23,6 @@ PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := fire
 PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi 12
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_fire-user 12 TP1A.220624.014 V14.0.13.0.TMXMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_fire/fire:12/TP1A.220624.014/V14.0.13.0.TMXMIXM:user/release-keys
