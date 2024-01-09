2f8845885b29ee9f4580799f6b7d97974ed1881d
YudhoPatrianto <kydh01123@gmail.com>
2024-01-10 04:00:42 +0700

    fire: Add fstab entry
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/first_stage_ramdisk/fstab.mt6768
+
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
